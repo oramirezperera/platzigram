f432156a4b5528aaf17802db54e0050bc7ed6b9d
oramirezperera <ramirez.orlando17@gmail.com>
2021-02-15 11:50:18 -0400

    changes to views and urls now if you pass number=<some number> through the url you can see the sorted numbers in the page

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from platzigram import views
+
 urlpatterns = [
-    path('hello-world/', hello_world)
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi),
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -1,8 +1,21 @@
 """Platzigram views"""
-from django.http import HttpResponse
 
+#Django
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+# utilities
+from datetime import datetime
 
 def hello_world(request):
     """returns a greeting"""
-    return HttpResponse('Hello, World!')
+    now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    return HttpResponse(f'Hello, World! The datetime of the server is {now}')
+
+
+def hi(request):
+    """hi"""
+    numbers = sorted([int(number) for number in request.GET['numbers'].split(',')])
+
+    return JsonResponse(numbers, safe=False)
 
