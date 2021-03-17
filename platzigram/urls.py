2f59ef46866964f6b66954bf4f85072c0dab7517
oramirezperera <ramirez.orlando17@gmail.com>
2021-03-17 18:35:48 -0400

    erased previous migrations and all the models.py, we are using now the models and users from django

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#Django
 from django.contrib import admin
 from django.urls import path
 
@@ -20,6 +21,8 @@ from platzigram import views as local_views
 from posts import views as posts_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
