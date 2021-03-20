bcc082a83cd099ca41ac3c5701ed3891ac839b37
oramirezperera <ramirez.orlando17@gmail.com>
2021-03-20 16:04:40 -0400

    added some changes to posts app

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from posts.models import Posts
 
-# Register your models here.
+# Register your models here
+
+
+@admin.register(Posts)
+class PostAdmin(admin.ModelAdmin):
+    """Posts admin model"""
+
+    list_display = ('pk', 'user', 'photo')
+    list_display_links = ('pk', 'user')
+    list_editable = ('photo',)
+    list_filter = ('created', 'modified')
