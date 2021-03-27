4307a94987210f59143808e1da5fd38bb9e0c7aa
oramirezperera <ramirez.orlando17@gmail.com>
2021-03-26 22:13:14 -0400

    major changes to the nav.html and changes to show and use the icons to go back to feed add a new post and to show all the new posts, with the timestamp and the user posting

---
+++ posts/forms.py
@@ -0,0 +1,16 @@
+""" Post Forms """
+
+# Django
+from django import forms
+
+# Models
+from posts.models import Posts
+
+
+class PostForm(forms.ModelForm):
+    """ Post model form """
+    class Meta:
+        """ Form settings """
+
+        model = Posts
+        fields = ('user', 'profile', 'title', 'photo')
