87c643cbe2403b5421b987aa6b2fd5eff62eeabd
oramirezperera <ramirez.orlando17@gmail.com>
2021-03-27 22:25:10 -0400

    changes to the signup forms and views, now uses the django classes

--- users/views.py
+++ users/views.py
@@ -5,15 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Models
-from django.contrib.auth.models import User
-from users.models import Profile
-
-# Exception
-from django.db.utils import IntegrityError
-
 # Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 # Create your views here.
 
 
@@ -32,7 +25,7 @@ def update_profile(request):
             profile.biography = data['biography']
             profile.picture = data['picture']
             profile.save()
-            
+
             return redirect('update_profile')
     else:
         form = ProfileForm()
@@ -59,7 +52,8 @@ def login_view(request):
             login(request, user)
             return redirect('feed')
         else:
-            return render(request, 'users/login.html', {'error': 'Invalid username and/or password'})
+            return render(request, 'users/login.html',
+                          {'error': 'Invalid username and/or password'})
 
     return render(request, 'users/login.html')
 
@@ -67,28 +61,17 @@ def login_view(request):
 def signup(request):
     """ sign up for new users """
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
-
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-    return render(request, 'users/signup.html')
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form}
+    )
 
 
 @login_required
